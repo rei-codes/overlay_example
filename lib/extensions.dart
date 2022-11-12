7ee62fcf248f14b4bd96b2edcb7fe127dcfdc11c
Hasan Basri Bayat <hasanbb1567@gmail.com>
2022-11-12 19:17:41 +0300

    extensions params converted to named params

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -4,10 +4,10 @@ import 'toast_widget.dart';
 
 extension ToastExtension on BuildContext {
   void showToast(
-    String text, [
+    String text, {
     Duration duration = const Duration(seconds: 3),
     Duration transitionDuration = const Duration(milliseconds: 250),
-  ]) {
+  }) {
     // Get the OverlayState
     final overlayState = Overlay.of(this);
     // Create an OverlayEntry with your custom widget
